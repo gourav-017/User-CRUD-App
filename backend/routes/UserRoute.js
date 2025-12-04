3661c8b1f28e15be7cf6f9e300a2b36c5a34d21c
Gourav Singh <gouravrajput23110@gmail.com>
2025-12-04 21:33:42 +0530

    Update UserRoute.js

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers, createUser, updateUser, deleteUser } from "../Controllers/UserController.js";
+import { getUsers, createUser, updateUser, deleteUser } from "../controllers/UserController.js";
 
 const router = express.Router();
 
